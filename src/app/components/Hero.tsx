bfa94196f9af5d30542d6dc34f2fa97bf1747143
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-11-09 18:58:19 +0500

    initialized

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className='flex flex-col text-center text-xl md:text-6xl gap-4 justify-center h-screen'>
         <h1 className=''>Check All the other assignments please...</h1>
-        <h1 className=''>I'll complete this soon</h1>
+        <h1 className=''>I will complete this soon</h1>
     </div>
   )
 }
