2895b187da2c2b20572bd985c58e87fae8a89d1f
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-11-09 18:51:35 +0500

    initialized

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex flex-col text-center text-xl md:text-6xl gap-4 justify-center h-screen'>
+        <h1 className=''>Check All the other assignments please...</h1>
+        <h1 className=''>I'll complete this soon</h1>
+    </div>
+  )
+}
+
+export default Hero
